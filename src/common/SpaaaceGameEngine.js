4a36d1400876ba44e2ae3c30d3a6b300880f3d3a
David Seaman <davidroyseaman@gmail.com>
2018-06-22 17:07:38 +1200

    Attempt at bounce

--- src/common/SpaaaceGameEngine.js
+++ src/common/SpaaaceGameEngine.js
@@ -6,6 +6,29 @@ import Ship from './Ship';
 import Missile from './Missile';
 import TwoVector from 'lance/serialize/TwoVector';
 
+// http://www.euclideanspace.com/physics/dynamics/collision/twod/index.htm#code
+const collisionResponse = (e, ma, mb, Ia, Ib, ra, rb, n,
+     vai, vbi, wai, wbi, vaf, vbf, waf, wbf) => {
+
+  const k=1/(ma*ma)+ 2/(ma*mb) +1/(mb*mb) - ra.x*ra.x/(ma*Ia) - rb.x*rb.x/(ma*Ib) - ra.y*ra.y/(ma*Ia)
+    - ra.y*ra.y/(mb*Ia) - ra.x*ra.x/(mb*Ia) - rb.x*rb.x/(mb*Ib) - rb.y*rb.y/(ma*Ib)
+    - rb.y*rb.y/(mb*Ib) + ra.y*ra.y*rb.x*rb.x/(Ia*Ib) + ra.x*ra.x*rb.y*rb.y/(Ia*Ib) - 2*ra.x*ra.y*rb.x*rb.y/(Ia*Ib);
+
+  const Jx = (e+1)/k * (vai.x - vbi.x) * ( 1/ma - ra.x*ra.x/Ia + 1/mb - rb.x*rb.x/Ib)
+     - (e+1)/k * (vai.y - vbi.y) * (ra.x*ra.y / Ia + rb.x*rb.y / Ib);
+
+  const Jy = -1 * (e+1)/k * (vai.x - vbi.x) * (ra.x*ra.y / Ia + rb.x*rb.y / Ib)
+     + (e+1)/k * (vai.y - vbi.y) * ( 1/ma - ra.y*ra.y/Ia + 1/mb - rb.y*rb.y/Ib);
+  vaf.x = vai.x - Jx/Ma;
+  vaf.y = vai.y - Jy/Ma;
+  vbf.x = vbi.x - Jx/Mb;
+  vbf.y = vbi.y - Jy/Mb;
+  waf.x = wai.x - (Jx*ra.y - Jy*ra.x) /Ia;
+  waf.y = wai.y - (Jx*ra.y - Jy*ra.x) /Ia;
+  wbf.x = wbi.x - (Jx*rb.y - Jy*rb.x) /Ib;
+  wbf.y = wbi.y - (Jx*rb.y - Jy*rb.x) /Ib;
+};
+
 export default class SpaaaceGameEngine extends GameEngine {
 
     constructor(options) {
@@ -18,12 +41,12 @@ export default class SpaaaceGameEngine extends GameEngine {
         });
     }
 
-    registerClasses(serializer){
+    registerClasses(serializer) {
         serializer.registerClass(Ship);
         serializer.registerClass(Missile);
     }
 
-    initWorld(){
+    initWorld() {
         super.initWorld({
             worldWrap: true,
             width: 3000,
@@ -36,20 +59,92 @@ export default class SpaaaceGameEngine extends GameEngine {
 
         this.on('collisionStart', e => {
             let collisionObjects = Object.keys(e).map(k => e[k]);
-            let ship = collisionObjects.find(o => o instanceof Ship);
-            let missile = collisionObjects.find(o => o instanceof Missile);
+            let ships = collisionObjects.filter(o => o instanceof Ship);
+            // let missile = collisionObjects.find(o => o instanceof Missile);
 
-            if (!ship || !missile)
-                return;
+
+            // if (!ship || !missile)
+            //     return;
 
             // make sure not to process the collision between a missile and the ship that fired it
-            if (missile.playerId !== ship.playerId) {
-                this.destroyMissile(missile.id);
-                this.trace.info(() => `missile by ship=${missile.playerId} hit ship=${ship.id}`);
-                this.emit('missileHit', { missile, ship });
-            }
+            // if (missile.playerId !== ship.playerId) {
+            //     this.destroyMissile(missile.id);
+            //     this.trace.info(() => `missile by ship=${missile.playerId} hit ship=${ship.id}`);
+            //     this.emit('missileHit', { missile, ship });
+            // }
         });
 
+        this.on('preStep', () => {
+            let ships = this.world.queryObjects({
+                instanceType: Ship
+            });
+            for (let i = 0; i < ships.length - 1; i++) {
+                for (let j = i + 1; j < ships.length; j++) {
+                    const a = ships[i];
+                    const b = ships[j];
+                    a.r=50; b.r=50;
+                    a.mass = 1; b.mass = 1;
+
+                    const distance = Math.hypot(a.x-b.x, a.y-b.y);
+                    const width = (a.r + b.r);
+
+                    // Check a circle collision actually happened
+                    if (distance > width) { continue; }
+                    console.log('boing');
+
+                    const collisionX = ((a.x * b.r) + (b.x * a.r)) / (a.r + b.r);
+                    const collisionY = ((a.y * b.r) + (b.y * a.r)) / (a.r + b.r);
+
+
+                    // @param double e coefficient of restitution which depends on the nature of the two colliding materials
+                    const e = 0.5;
+                    // @param double ma total mass of body a
+                    const ma = a.mass;
+                    // @param double mb total mass of body b
+                    const mb = b.mass;
+                    // @param double Ia inertia for body a.
+                    const Ia = 1;
+                    // @param double Ib inertia for body b.
+                    const Ib = 1;
+                    // @param vector ra position of collision point relative to centre of mass of body a in absolute coordinates (if this is
+                    //                  known in local body coordinates it must be converted before this is called).
+                    const ra = { x: collisionX - a.x, y: collisionY - a.y };
+                    // @param vector rb position of collision point relative to centre of mass of body b in absolute coordinates (if this is
+                    //                  known in local body coordinates it must be converted before this is called).
+                    const rb = { x: collisionX - b.x, y: collisionY - b.y };
+                    // @param vector n normal to collision point, the line along which the impulse acts.
+                    const n = { x: (a.x - b.x)/distance, y: (a.y - b.y)/distance };
+                    // @param vector vai initial velocity of centre of mass on object a
+                    const vai = { x: a.velocity.x, y: a.velocity.y };
+                    // @param vector vbi initial velocity of centre of mass on object b
+                    const vbi = { x: b.velocity.x, y: b.velocity.y };
+                    // @param vector wai initial angular velocity of object a
+                    const wai = 0;
+                    // @param vector wbi initial angular velocity of object b
+                    const wbi = 0;
+                    // @param vector vaf final velocity of centre of mass on object a
+                    const vaf = {};
+                    // @param vector vbf final velocity of centre of mass on object a
+                    const vbf = {};
+                    // @param vector waf final angular velocity of object a
+                    const waf = {};
+                    // @param vector wbf final angular velocity of object b
+                    const wbf = {};
+
+                    collisionResponse(e, ma, mb, Ia, Ib, ra, rb, n, vai, vbi, wai, wbi, vaf, vbf, waf, wbf);
+
+                    a.velocity.x = vaf.x;
+                    a.velocity.y = vaf.y;
+                    b.velocity.x = vbf.x;
+                    b.velocity.y = vbf.y;
+
+                }
+            }
+            for (let k = 0; k < ships.length - 1; k++) {
+                ships[k].velocity.x *= 0.98;
+                ships[k].velocity.y *= 0.98;
+            }
+        });
         this.on('postStep', this.reduceVisibleThrust.bind(this));
     };
 
--- src/common/puck.js
+++ src/common/puck.js
@@ -4,13 +4,10 @@ import Renderer from '../client/SpaaaceRenderer';
 import Utils from './Utils';
 import ShipActor from '../client/ShipActor';
 
-export default class Ship extends DynamicObject {
+export default class Puck extends DynamicObject {
 
     constructor(gameEngine, options, props) {
         super(gameEngine, options, props);
-        this.showThrust = 0;
-        this.width = 100;
-        this.height = 100;
     }
 
     get maxSpeed() { return 3.0; }
@@ -35,15 +32,6 @@ export default class Ship extends DynamicObject {
 
     onRemoveFromWorld(gameEngine) {
 
-        if (this.fireLoop) {
-            this.fireLoop.destroy();
-        }
-
-        if (this.onPreStep){
-            this.gameEngine.removeListener('preStep', this.onPreStep);
-            this.onPreStep = null;
-        }
-
         let renderer = Renderer.getInstance();
         if (renderer) {
             if (gameEngine.isOwnedByPlayer(this)) {
@@ -56,7 +44,6 @@ export default class Ship extends DynamicObject {
                 if (sprite.actor) {
                     // removal "takes time"
                     sprite.actor.destroy().then(()=>{
-                        console.log('deleted sprite');
                         delete renderer.sprites[this.id];
                     });
                 } else {
@@ -77,33 +64,16 @@ export default class Ship extends DynamicObject {
     }
 
     toString() {
-        return `${this.isBot?'Bot':'Player'}::Ship::${super.toString()}`;
+        return `Player::Ship::${super.toString()}`;
     }
 
     syncTo(other) {
         super.syncTo(other);
-        this.showThrust = other.showThrust;
+        // this.showThrust = other.showThrust;
     }
 
 
     destroy() {
-    }
-
-    attachAI() {
-        this.isBot = true;
-
-        this.onPreStep = () => {
-            this.steer();
-        };
-
-        this.gameEngine.on('preStep', this.onPreStep);
-
-        let fireLoopTime = Math.round(250 + Math.random() * 100);
-        this.fireLoop = this.gameEngine.timer.loop(fireLoopTime, () => {
-            if (this.target && this.distanceToTargetSquared(this.target) < 160000) {
-                this.gameEngine.makeMissile(this);
-            }
-        });
     }
 
     shortestVector(p1, p2, wrapDist) {
@@ -117,44 +87,5 @@ export default class Ship extends DynamicObject {
         let dx = this.shortestVector(this.position.x, target.position.x, this.gameEngine.worldSettings.width);
         let dy = this.shortestVector(this.position.y, target.position.y, this.gameEngine.worldSettings.height);
         return dx * dx + dy * dy;
-    }
-
-    steer() {
-        return;
-        let closestTarget = null;
-        let closestDistance2 = Infinity;
-        for (let objId of Object.keys(this.gameEngine.world.objects)) {
-            let obj = this.gameEngine.world.objects[objId];
-            if (obj != this) {
-                let distance2 = this.distanceToTargetSquared(obj);
-                if (distance2 < closestDistance2) {
-                    closestTarget = obj;
-                    closestDistance2 = distance2;
-                }
-            }
-        }
-
-        this.target = closestTarget;
-
-        if (this.target) {
-
-            let newVX = this.shortestVector(this.position.x, this.target.position.x, this.gameEngine.worldSettings.width);
-            let newVY = this.shortestVector(this.position.y, this.target.position.y, this.gameEngine.worldSettings.height);
-            let angleToTarget = Math.atan2(newVX, newVY)/Math.PI* 180;
-            angleToTarget *= -1;
-            angleToTarget += 90; // game uses zero angle on the right, clockwise
-            if (angleToTarget < 0) angleToTarget += 360;
-            let turnRight = this.shortestVector(this.angle, angleToTarget, 360);
-
-            if (turnRight > 4) {
-                this.isRotatingRight = true;
-            } else if (turnRight < -4) {
-                this.isRotatingLeft = true;
-            } else {
-                this.isAccelerating = true;
-                this.showThrust = 5;
-            }
-
-        }
     }
 }
